20ae5c395e0582b52916c61f78f401b2c9c5ecc1
Rafael Belo <rafaelrpbelo@gmail.com>
2017-06-01 17:27:39 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
-# TODO
+cp vimrc ~/.vimrc
+cp zshrc ~/.zshrc
+cp zprofile ~/.zprofile
+cp gitconfig ~/.gitconfig
+cp tmux.conf ~/.tmux.conf
+cp xinitrc ~/.xinitrc
+cp Xresources ~/.Xresources
+cp config/i3/config ~/.config/i3/config
+cp config/i3status/config ~/.config/i3status/config
+logout
